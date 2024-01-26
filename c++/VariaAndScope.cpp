5de33543130b6c042d94f1b2dcdffb2b5b320989
Sid <siddhananjay@gmail.com>
2024-01-26 11:18:25 +0530

    26-1-24

---
+++ c++/VariaAndScope.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    // int a = 5;
+    // int b = 7;
+    int a = 5, b = 7;
+    float pi = 3.14;
+    char d = 'd';
+    cout<<"Theh value of a is  " << a << "  The value of int b is  " << b << "\n";
+    cout<<pi << "\n";
+    cout<<d;
+    return 0;
+}
