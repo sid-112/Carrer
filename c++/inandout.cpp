33aa5bf2160aa94208d4dfd37db355ac06ac5917
Sid <siddhananjay@gmail.com>
2024-01-26 11:18:25 +0530

    26-1-24

---
+++ c++/inandout.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    cout<<"The value of num1 - ";
+    cin>>num1;
+    
+    cout<<"The value of num2 - ";
+    cin>>num2;
+
+    cout<<"The sum of num1 and num2 is -:\n";
+    cout<<num1+num2;
+    return 0;
+}
