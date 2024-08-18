c453dff3bc7e8ad18cc03355b351ff506ba0f30a
Sid <siddhananjay@gmail.com>
2024-08-18 16:50:07 +0530

    18-08-2024

---
+++ Game/src/Learning_game_devlopment/Starter.java
@@ -0,0 +1,20 @@
+package Learning_game_development;
+
+import javax.swing.JFrame;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        // Create a JFrame window
+        JFrame window = new JFrame("Game Window");
+
+        // Set the window size
+        window.setSize(800, 600);
+
+        // Set the window to be visible
+        window.setVisible(true);
+
+        // Implement game logic here
+        // ...
+    }
+}
---
+++ Java/PracticeExersices/tangTangTanaTangTangTangaTangTangTangTangTanaNaTang.java
@@ -0,0 +1,29 @@
+package com.PracticeExersices;
+import java.util.Scanner;
+
+public class tangTangTanaTangTangTangaTangTangTangTangTanaNaTang {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to sigma calci");
+        System.out.println("Onyl arethmethic no algebric");
+        int uppNumber = sc.nextInt();
+        int downNumber = sc.nextInt();
+        int problem = sc.nextInt();
+        System.out.println(uppNumber);
+        System.out.println("__");
+        System.out.print("\\");;
+        System.out.println("//");
+        System.out.println("__");
+        System.out.println(downNumber);
+        System.out.println("is it correct");
+        String in = sc.next();
+        if (in == "ok"){
+            System.out.println("done");
+
+        }
+
+        else {
+            System.out.println("Bye");
+        }
+    }
+}
