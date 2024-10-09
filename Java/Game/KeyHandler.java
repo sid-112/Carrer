c6c50262b0c643b7b7049d6620509a4a3d75327a
Sid <siddhananjay@gmail.com>
2024-10-09 09:41:57 +0530

    9-10-2024

--- Java/Game/KeyHandler.java
+++ Java/Game/KeyHandler.java
@@ -6,7 +6,9 @@ import java.awt.event.KeyListener;
 public class KeyHandler implements KeyListener {
     public boolean upPressed, downPressed, leftPressed, rightPressed;
     @Override
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
--- Notepadd ++ Java/filehandle.java
+++ Notepadd ++ Java/filehandle.java
@@ -1,5 +1,5 @@
 public class filehandle"{
 	public static void main(String args[]){
-		int a = 1;
+		
 	}
 }
